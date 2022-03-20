732d7415ea1beafdb35482673b24a1dd7ea093ea
JesusStan <tipicultbiomassa@gmail.com>
2022-03-21 00:25:57 +0300

    error wrapper

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,10 +1,12 @@
 package telegram
 
 import (
-	"fmt"
 	"net/http"
 	"net/url"
+	"path"
 	"strconv"
+
+	"lib/e"
 )
 
 type Client struct {
@@ -32,16 +34,23 @@ func (c *Client) Updates(offset int, limit int) ([]Update, error) {
 
 }
 
-func (c *Client) doRequest(method string, query url.Values) ([]byte, error){
-	u:= url.URL{
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() { err = e.WrapIfErr(errMsg, err) }()
+	const errMsg = "Can't do request"
+	u := url.URL{
 		Scheme: "https",
-		Host: c.host
-		Path: path.join(c.basePath, method),
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
 	}
 	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("Can't do request: %w", err)
+		return nil, err
 	}
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+
+	defer func() { _ = resp.Body.Close() }
 }
 
 func (c *Client) SendMessage() {
